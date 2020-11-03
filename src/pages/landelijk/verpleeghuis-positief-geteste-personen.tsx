cc059ae26bfc5233e732291d4ac77fc9df87cff9
K <73584448+VWSCoronaDashboard10@users.noreply.github.com>
2020-11-03 11:06:04 +0100

    Update chart line data

--- src/pages/landelijk/verpleeghuis-positief-geteste-personen.tsx
+++ src/pages/landelijk/verpleeghuis-positief-geteste-personen.tsx
@@ -6,7 +6,9 @@ import { getNationalLayout } from '~/components/layout/NationalLayout';
 import { SEOHead } from '~/components/seoHead';
 import siteText from '~/locale/index';
 import getNlData, { INationalData } from '~/static-props/nl-data';
-import { formatNumber } from '~/utils/formatNumber';
+import { KpiTile } from '~/components-styled/kpi-tile';
+import { KpiValue } from '~/components-styled/kpi-value';
+import { TwoKpiSection } from '~/components-styled/two-kpi-section';
 
 const text = siteText.verpleeghuis_positief_geteste_personen;
 
@@ -30,24 +32,19 @@ const NursingHomeInfectedPeople: FCWithLayout<INationalData> = ({ data }) => {
         }}
       />
 
-      <article className="metric-article layout-two-column">
-        <div className="column-item column-item-extra-margin">
-          <h3>{text.barscale_titel}</h3>
-          <p className="text-blue kpi" data-cy="infected_daily_total">
-            {formatNumber(data.nursing_home.last_value.newly_infected_people)}
-          </p>
-        </div>
-
-        <div className="column-item column-item-extra-margin">
-          <p>{text.extra_uitleg}</p>
-        </div>
-      </article>
+      <TwoKpiSection>
+        <KpiTile title={text.barscale_titel} description={text.extra_uitleg}>
+          <KpiValue
+            absolute={data.nursing_home.last_value.newly_infected_people}
+          />
+        </KpiTile>
+      </TwoKpiSection>
 
       <article className="metric-article">
         <LineChart
           title={text.linechart_titel}
           values={data.nursing_home.values.map((value) => ({
-            value: value.newly_infected_locations,
+            value: value.newly_infected_people,
             date: value.date_of_report_unix,
           }))}
         />
